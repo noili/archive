3c0e2b20a34f77bef14d9c94b65af40e74cdd17d
noili <federico.neuman@gmail.com>
2014-09-18 18:43:47 -0300

    Pimp StepTest.

--- test/models/step_test.rb
+++ test/models/step_test.rb
@@ -2,28 +2,33 @@ require 'test_helper'
 
 class StepTest < ActiveSupport::TestCase
   
+  def office
+    offices :computo
+  end
+  
+  def person
+    people :emmet
+  end
+    
   test "create step with existant office" do
-    office = offices :computo
     step = Step.create office: office
     assert step.office.name == 'Computo'
   end
   
-  test "create step with inexistant office" do
+  test "should not create step with inexistant office" do
     step = Step.new
     assert !step.valid?
   end
   
   test "create step with email" do
-    office = offices :computo
-    person = people :emmet
     step = Step.create office: office, email: person.email
     assert step.person == person
   end
   
   test "create step an person with email" do
-    office = offices :computo
     step = Step.create office: office, email: 'inexistant@mail.com'
     assert step.person.email == 'inexistant@mail.com'
   end
   
+
 end

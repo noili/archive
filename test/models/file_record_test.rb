e28a26c5fb8132a65554970047ea31f78584e56b
noili <federico.neuman@gmail.com>
2014-09-18 18:20:28 -0300

    Add test for start_office and archive_office.

--- test/models/file_record_test.rb
+++ test/models/file_record_test.rb
@@ -9,13 +9,13 @@ class FileRecordTest < ActiveSupport::TestCase
   
   test "when creating file record first step should be mesa de entrada" do
     file_record = FileRecord.create
-    assert file_record.steps.first.office.name == 'Mesa de Entrada'
+    assert file_record.steps.first.office == Office.start_office
   end
   
   test "when archivating file record last step should be archivate" do
     file_record = FileRecord.create
     file_record.archivate
-    assert file_record.steps.last.office.name == 'Archivo'
+    assert file_record.steps.last.office == Office.archive_office
   end
   
 end
--- test/models/office_test.rb
+++ test/models/office_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class OfficeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should return the first office a file record should be in" do
+    assert Office.start_office.name == 'Mesa de Entrada'
+  end
+  
+  test "should return the last possible office a file record can be in" do
+    assert Office.archive_office.name == 'Archivo'
+  end
+  
 end
